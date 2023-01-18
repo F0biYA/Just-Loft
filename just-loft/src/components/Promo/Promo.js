8368043e589eb28bfc0bb81371d8af74aa4a0cb6
F0BiYa <fobiya1982@yandex.ru>
2023-01-18 13:23:36 +0300

    fix title

--- just-loft/src/components/Promo/Promo.js
+++ just-loft/src/components/Promo/Promo.js
@@ -5,10 +5,10 @@ function Promo() {
 
     return (
         <section id='промо' className='promo'>
-            <div className='promo__cover'></div>
+            <div className='promo__cover'>
             {/* <div className='promo__logo'></div> */}
             <h1 className='promo__title neonText'>JuSt LOFt</h1>
-            <h2 className='promo__subtitle'>Здесь просто отдыхают</h2>
+            <h2 className='promo__subtitle'>Здесь просто отдыхают</h2></div>
 
         </section>
     )
